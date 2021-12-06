cd06a42306bc1db0601ce02786570468818ade74
RainReagan <83326575+RainReagan@users.noreply.github.com>
2021-12-06 12:21:05 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer>
+    <div class="footer-cont">
+        <div class="title">OUR CATALOGS</div>
+        <p><a href="#">Sneakers</a></p>
+        <p><a href="#">Boots</a></p>
+        <p><a href="#">Slip-ons</a></p>
+        <p><a href="#">Hikings</a></p>
+        <p><a href="#">Oxfords</a></p>
+        <p><a href="#">Wedges</a></p>
+        <p><a href="#">High heels (Stilettos)</a></p>
+        <p><a href="#">High-tops</a></p>
+        <p><a href="#">Sandals</a></p>
+        <p><a href="#">Sports</a></p>
+    </div>
+    <div class="footer-cont">
+        <div class="title">PAGES</div>
+        <p><a href="#">Home</a></p>
+        <p><a href="#">About Us</a></p>
+        <p><a href="#">Login</a></p>
+        <p><a href="#">Register</a></p>
+    </div>
+    <div class="footer-cont">
+        <div class="title">ACCOUNT</div>
+        <p><a href="#">Track Order</a></p>
+        <p><a href="#">Manage Account</a></p>
+        <p><a href="#">Order History</a></p>
+        <br>
+        <div class="title">LEGALS</div>
+        <p><a href="privacy_policy.php">Privacy Policy</a></p>
+        <p><a href="refund.php">Refunds</a></p>
+        <p><a href="t&c.php">Term and Conditions</a></p>
+    </div>
+    <div class="footer-cont">
+        <div class="title">Need Customer Support? Contact us</div>
+        <p><b>Phone number:</b> (082)446753181</p>
+        <p><b>e-Mail:</b> cs.shoes@gmail.com</p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,3 +1,103 @@
 <?php
-    header("Location:../proyekPW/php/index.php");
+    require_once("koneksi.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../java/jquery.min.js"></script>
+    <script src="../java/jquery.autocomplete.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="../java/java.js"></script>
+    <style>
+        .showcase .right .button a{
+            color: white;
+            text-decoration: none;
+        }
+        .showcase .right .button a:visited{
+            color: white;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <!-- navbar -->
+        <?php require_once("navbar.php"); ?>
+        <!-- jumbotron -->
+        <div class="jumbotron">
+            <div class="left">&#10094;</div>
+            <div class="jumbo-wrap">
+                <p class="caption">Shoes For You</p>
+                
+                <div class="shopnow">SHOP NOW</div>
+                
+            </div>
+            <div class="right">&#10095;</div>
+        </div>
+        <!-- categories -->
+        <div class="categories">
+            <div class="cate-wrap">
+                <div class="category a">
+                    <div class="wrapper">
+                        <div class="text">Casual</div>
+                        <div class="button">Shop Now</div>
+                    </div>
+                </div>
+                <div class="category b">
+                    <div class="wrapper">
+                        <div class="text">Sports</div>
+                        <div class="button">Shop Now</div>
+                    </div>
+                </div>
+                <div class="category c">
+                    <div class="wrapper">
+                        <div class="text">Formal</div>
+                        <div class="button">Shop Now</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- showcase picture left -->
+        <div class="showcase">
+            <div class="left">
+            </div>
+            <div class="right">
+            <div class="title">Discount Shoes!</div>
+                <div class="text">Discount Shoes, just for you! Only today! Don't miss this chance! Take a look by clicking this button below!</div>
+                <div class="button" name="btnDisc">Go now</div>
+            </div>
+        </div>
+        <!-- showcase picture right -->
+        <div class="showcase">
+            <div class="right">
+                <div class="title">About Us</div>
+                <div class="text">Want to know more about our shoes store? Click this button below!</div>
+                <div class="button" name="btnAbout"><a href="about.php">Learn more about our shop!</a></div>
+            </div>
+            <div class="left">
+            </div>
+        </div>
+        <?php require_once("../php/footer.php"); ?>
+    </div>
+    <script>
+        $(document).ready(function(){
+            $(".button").click(function (e) { 
+                var category = $(this).parent().children().first().html().replace(" ","+");
+                window.location.href="../php/productlist.php?category="+category;
+                
+            });
+            $(".shopnow").click(function (e) {
+                window.location.href="../php/productlist.php?category=all";
+            });
+            
+        });
+    </script>
+</body>
+</html>
